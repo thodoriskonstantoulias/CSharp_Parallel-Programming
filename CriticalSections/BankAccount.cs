aeca860a272d7bb7b77526df51486fdfd4eca554
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-24 18:29:35 +0200

    Interlocked instead of Lock

--- CriticalSections/BankAccount.cs
+++ CriticalSections/BankAccount.cs
@@ -1,27 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace CriticalSections
 {
+    // interlocked class contains atomic operations on variables
+    // atomic = cannot be interrupted
     public class BankAccount
     {
         public object padLock = new object();
-        public int Balance { get; private set; }
+        private int balance;
+        public int Balance { get { return balance; } private set { balance = value; } }
 
+        //public void Deposit(int amount)
+        //{
+        //    lock (padLock) 
+        //    { 
+        //        Balance += amount;
+        //    }
+        //}
+        //public void Withdraw(int amount)
+        //{
+        //    lock (padLock)
+        //    {
+        //        Balance -= amount;
+        //    }
+        //}
+
+        //Introducing Interlocked operations
         public void Deposit(int amount)
         {
-            lock (padLock) 
-            { 
-                Balance += amount;
-            }
+            Interlocked.Add(ref balance, amount);
         }
         public void Withdraw(int amount)
         {
-            lock (padLock)
-            {
-                Balance -= amount;
-            }
+            Interlocked.Add(ref balance, -amount);
         }
     }
 }
