6795a8446912bdc8c90244dff63d17b12b81a579
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-24 18:21:32 +0200

    Intoducing Lock for sync the Tasks

---
+++ CriticalSections/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CriticalSections
+{
+    public class BankAccount
+    {
+        public object padLock = new object();
+        public int Balance { get; private set; }
+
+        public void Deposit(int amount)
+        {
+            lock (padLock) 
+            { 
+                Balance += amount;
+            }
+        }
+        public void Withdraw(int amount)
+        {
+            lock (padLock)
+            {
+                Balance -= amount;
+            }
+        }
+    }
+}
---
+++ CriticalSections/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CriticalSections
+{
+    class Program
+    {
+        //In our first try the desired result is not working because the methods are not atomic 
+        //meaning that values are changing between tasks (simultaneously computations)
+        //Intoducing lock corrects the behaviour (check BankAccount class)
+        static void Main(string[] args)
+        {           
+            var tasks = new List<Task>();
+
+            var ba = new BankAccount();
+
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        ba.Deposit(100);
+                    }
+                }));
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        ba.Withdraw(100);
+                    }
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine($"Final balance is {ba.Balance}");
+            Console.ReadKey();
+        }
+    }
+}
