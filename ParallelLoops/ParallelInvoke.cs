bc6a067e6344f4a9eec49b94b6fa432f8901fe37
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-26 21:41:44 +0200

    Parallel Invoke,For,Foreach

---
+++ ParallelLoops/ParallelInvoke.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelLoops
+{
+    public class ParallelInvoke
+    {
+        
+        public static void ExecuteMethod()
+        {
+            var a = new Action(() => { Console.WriteLine($"First : {Task.CurrentId}"); });
+            var b = new Action(() => { Console.WriteLine($"Second : {Task.CurrentId}"); });
+            var c = new Action(() => { Console.WriteLine($"Third : {Task.CurrentId}"); });
+
+            Parallel.Invoke(a, b, c);
+
+            Parallel.For(1, 11, i =>
+             {
+                 Console.WriteLine($"{i*i}\t");
+             });
+
+            string[] words = {"one", "two", "three", "four" };
+            Parallel.ForEach(words, word =>
+            {
+                Console.WriteLine($"{word} has length {word.Length} ,(task = {Task.CurrentId})");
+            });
+        }
+    }
+}
---
+++ ParallelLoops/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ParallelLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Introducing Parallel Programming, Invoke, For, Foreach
+            ParallelInvoke.ExecuteMethod();
+
+            Console.ReadKey();
+        }
+    }
+}
