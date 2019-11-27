db75e41371df69cc618a1236c8a0d791e63bf15f
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-27 21:43:40 +0200

    Parallel queries

---
+++ ParallelLINQ/ParalQuery.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelLINQ
+{
+    public class ParalQuery
+    {
+        public static void ExecuteMethod()
+        {
+            const int count = 50;
+            var items = Enumerable.Range(1, count).ToArray();
+            var results = new int[count];
+
+            items.AsParallel().ForAll(x =>
+            {
+                int newValue = x * x * x;
+                Console.WriteLine($"{newValue} with task id : {Task.CurrentId}");
+                results[x - 1] = newValue;
+            });
+            Console.WriteLine();
+
+            var cubes = items.AsParallel().AsOrdered().Select(x => x* x *x);
+            // this evaluation is delayed: you actually calculate the values only
+            // when iterating or doing ToArray()
+            foreach (var i in cubes)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ ParallelLINQ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ParallelLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Introducing AsParallel and ParallelQuery
+            ParalQuery.ExecuteMethod();
+
+            Console.ReadKey();
+        }
+    }
+}
