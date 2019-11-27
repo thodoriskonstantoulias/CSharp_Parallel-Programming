3d67850c8a6994b622171e6801ea07ff9aac8597
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-28 00:26:28 +0200

    Created WinfForm and practiced async/await showing the pros of usage

---
+++ AsyncAwaitFormExample/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace AsyncAwaitFormExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblresult = new System.Windows.Forms.Label();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(349, 151);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblresult
+            // 
+            this.lblresult.AutoSize = true;
+            this.lblresult.Location = new System.Drawing.Point(365, 87);
+            this.lblresult.Name = "lblresult";
+            this.lblresult.Size = new System.Drawing.Size(38, 15);
+            this.lblresult.TabIndex = 1;
+            this.lblresult.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(796, 517);
+            this.Controls.Add(this.lblresult);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblresult;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ AsyncAwaitFormExample/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwaitFormExample
+{
+    public partial class Form1 : Form
+    {
+        public int CalculateValue()
+        {
+            Thread.Sleep(5000);
+            return 123;
+        }
+        public Task<int> CalculateValueAsync()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(5000);
+                return 123;
+            });              
+        }
+        public async Task<int> CalculateValueAsyncBetterMethod()
+        {
+            await Task.Delay(5000);
+            return 123;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //The following regulat method blocks the UI
+            //int n = CalculateValue();
+            //lblresult.Text = n.ToString();
+
+            //Using asynchronous method give the UI freedom to move etc.
+            //var calculation = CalculateValueAsync();
+            //calculation.ContinueWith(t =>
+            //{
+            //    lblresult.Text = t.Result.ToString();
+            //},TaskScheduler.FromCurrentSynchronizationContext());
+
+            //Better method for asynchronous programming
+            int value = await CalculateValueAsyncBetterMethod();
+            lblresult.Text = value.ToString();
+
+            //And now let's play
+            await Task.Delay(5000);
+            using(var wc = new WebClient())
+            {
+                string data = await wc.DownloadStringTaskAsync("http://google.com/robots.txt");
+                lblresult.Text = data.Split('\n')[0].Trim();
+            }
+        }
+    }
+}
